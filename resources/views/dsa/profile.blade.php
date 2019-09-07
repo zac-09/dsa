7588d5e13f61dee21627e90d18b3d2208d17b787
isaacmmarvin <isaacmubiru99@gmail.com>
2019-09-07 16:35:11 +0300

    message controlle

--- resources/views/dsa/profile.blade.php
+++ resources/views/dsa/profile.blade.php
@@ -69,7 +69,7 @@
                 <div class="row">
                     <div class="col-md-4">
                         <div class="profile-img">
-                            <img src="/var/www/dsa/public/storage/profile_pics/{{ $user->profile_pic}}" alt=""/>
+                            <img src="{{ URL::to('storage/profile_pics/{{ $user->profile_pic}}') }}" alt=""/>
 
                         </div>
                     </div>
