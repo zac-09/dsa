59b69afa0fd3256990d57a58ec189c9315450aa6
isaacmmarvin <isaacmubiru99@gmail.com>
2019-09-10 11:15:56 +0300

    zac

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,13 +5,11 @@ use social\Models\Status;
 class HomeController extends Controller{
 
     public function index(){
-        $statuses = Status::where(function($query){
-            return $query->notReply();})->orderBY('created_at', 'desc')->paginate(10);
+
 
         if(Auth::check()){
             $statuses = Status::where(function($query){
-                    return $query->notReply()->where('user_id',Auth::user()->id);
-            })->orderBY('created_at','desc')->paginate(10);
+                return $query->notReply();})->orderBY('created_at', 'desc')->paginate(10);
 
 
             return view('dsa.index')->with('statuses',$statuses);
